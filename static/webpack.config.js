beb0a87896d4bb8eb868ae407946a3647348e5a0
Wong Wai Keat <waikeat@meta.us>
2021-01-16 15:55:43 +0800

    Initial website

---
+++ static/webpack.config.js
@@ -0,0 +1,89 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+    entry: ['./js/main.js', './scss/app.scss'],
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            cleanStaleWebpackAssets: false
+        }),
+        new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: 'img',
+                        to: 'images'
+                    },
+                    {
+                        from: 'webfonts',
+                        to: 'webfonts'
+                    }
+                ]
+            }
+        ),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: 'css/style.min.css',
+            chunkFilename: '[id].css'
+        }),
+    ],
+    output: {
+        filename: 'min_js/scripts.min.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: (resourcePath, context) => {
+                                // publicPath is the relative path of the resource to the context
+                                // e.g. for ./css/admin/main.css the publicPath will be ../../
+                                // while for ./css/main.css the publicPath will be ../
+                                return path.relative(path.dirname(resourcePath), context) + '/';
+                            },
+                        },
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'sass-loader'
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    }
+                ],
+            },
+            {
+              test: /\.(png|jpg)$/,
+              include: path.join(__dirname, 'img'),
+              loader: 'url-loader'
+            },
+            {
+                test: /\.(ttf|eot|woff|woff2|svg)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        publicPath: '../webfonts/',
+                        outputPath: '../webfonts/',
+                        context: '../webfonts/'
+                    },
+                },
+            },
+        ],
+    }
+};
